e6e839e1c3d08ec4c2b98bbea6be55afaf27d86c
Angel <thesparkingofhope@gmail.com>
2026-01-12 17:40:25 -0500

    Melee Wraith

--- Assets/Scripts/Wraith.cs
+++ Assets/Scripts/Wraith.cs
@@ -1,21 +1,52 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Wraith : MonoBehaviour
 {
     public int health;
     public Transform playerTransform;
-    
+    private float timer;
+    [SerializeField] float attackCooldown;
+    [SerializeField] float attackRange;
+    private float distance;
+    private bool attacking;
+    [SerializeField] GameObject attackIndicator;   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = 3;
         playerTransform = GameManager.Instance.player.transform;
+        timer = 0;
+        distance = 0;
+        attacking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         FacePlayer();
+        distance = (playerTransform.position - transform.position).magnitude;
+        if (timer > attackCooldown & distance < attackRange)
+        {
+            timer = 0;
+            attacking = true;
+            GameManager.Instance.player.applyDamage(20);
+        }
+        //Temp Code until we have animations, hence hardcoded numbers
+        if(timer>1 & attacking)
+        {
+            attacking = false;
+        }
+        if (attacking)
+        {
+            attackIndicator.SetActive(true);
+        }
+        else
+        {
+            attackIndicator.SetActive(false);
+        }
+        // End of "Temp" Code
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,8 +55,8 @@ public class Wraith : MonoBehaviour
         {
             --health;
             if (health <= 0)
-            {
-                Destroy(gameObject);
+            {               
+                Destroy(gameObject);    
             }
 
         }
